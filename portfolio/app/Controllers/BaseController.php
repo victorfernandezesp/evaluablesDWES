179ff10b4fdf673d8359f489ce2031f056caf0d4
victorfernandezesp <91657222+victorfernandezesp@users.noreply.github.com>
2024-03-10 20:13:09 +0100

    Delete portfolio directory

--- portfolio/app/Controllers/BaseController.php
+++
@@ -1,8 +0,0 @@
-<?php
-    namespace App\Controllers;
-
-    class BaseController {
-        public function renderHTML($fileName, $data = []) { // $fileName = String con la ruta del archivo a renderizar, $data = Array con los datos a renderizar
-            include($fileName);
-        }
-    }
